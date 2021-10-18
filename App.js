7cf8ffff5428ec53ea2d1de16402ba433b624ba3
khaledpasha <89224687+khaledpasha@users.noreply.github.com>
2021-10-18 22:19:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+// import Detail from './Detail';
+import Fetch from './Fetch';
+function App() {
+  return (
+    <div className="App">
+     <Fetch/>
+    </div>
+  );
+}
+
+export default App;
